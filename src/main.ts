35cd1d7250b3764f3bfb17afb84f8c8bcc861e51
eneyret <manu@holidee.fr>
2024-05-13 12:01:40 +0200

    Clean

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ let currentPopup: any = undefined;
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Miléade by Holidée \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Pour te rendre directement sur la terrasse, clique sur ce bouton :", [{
+		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue chez Holidée \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Pour te rendre directement sur la terrasse, clique sur ce bouton :", [{
 			label: "MARCHER VERS LA TERRASSE",
 			className: "primary",
 			callback: () => {
@@ -20,7 +20,7 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('welcome').subscribe(closePopup);
 
 	WA.room.area.onEnter('welcome').subscribe(() => {
-		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue au casting Miléade by Holidée \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Pour te rendre directement sur la terrasse, clique sur ce bouton :", [{
+		currentPopup = WA.ui.openPopup("welcomePopup", "Bienvenue chez Holidée \r \r La présentation se déroulera en visio principalement sur la terrasse \r \r Pour te rendre directement sur la terrasse, clique sur ce bouton :", [{
 			label: "MARCHER VERS LA TERRASSE",
 			className: "primary",
 			callback: () => {
